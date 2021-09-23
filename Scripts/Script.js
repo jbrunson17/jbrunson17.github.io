6aa61d08381dcda13b4c9306ca54580a535a4a8a
jbrunson17 <91283788+jbrunson17@users.noreply.github.com>
2021-09-23 13:20:58 -0500

    Add files via upload

---
+++ Scripts/Script.js
@@ -0,0 +1,17 @@
+//This code reads your mind
+
+
+alert("Pick any whole number between 1 and 10 then press OK");
+alert("First multiply the number you picked by 2 to continue");
+alert("Then Multiply your result by 5 and press OK");
+alert("Now divide your answer by the original number you picked in the beginning");
+alert("Finally, subtract 7 from the result");
+
+alert("Your Final answer is 3");
+
+
+
+
+
+
+
